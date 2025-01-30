c37fd61b5b64fe6fb6261d7b03f9d241cc81730d
Usman00111 <g00400065@atu.ie>
2025-01-30 04:19:04 +0000

    added a func to get all lecturers and added some comments with index page now has code which collects students from mysql and lecturer from mongodb

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-//imports
+//imports mysql and mongodb
 const express = require('express');
 const app = express();
+const mysqlDao = require('./models/mysqlDao');
+const mongoDao = require('./models/mongoDao');
 
 //embeded javascript used to create web pages and basically tells the express that all view files have a .ejs extention. (in view folder )
 app.set('view engine', 'ejs');
-
 //this is a middleware used to handle form submissions basically a post req & it parses forms data so we can read it 
 app.use(express.urlencoded({ extended: true }));
 
 //defining http get rq for the home page (/) and res.render will render views/home.ejs file as the response 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+//fetch students from mysql 
+app.get('/students', (req, res) => {
+    mysqlDao.getStudents()
+        .then((students) => res.render('students', {students}))
+        .catch((err) => res.send(err));
+});
+
+// Fetch lecturers from MongoDB
+app.get('/lecturers', (req, res) => {
+    mongoDao.getLecturers()
+        .then((lecturers) => res.render('lecturers', { lecturers}))
+        .catch((err) => res.send(err));
 });
 
 //starts the the server on port 3004 witht he message 
--- models/mongoDao.js
+++ models/mongoDao.js
@@ -1,15 +1,17 @@
 const { MongoClient } = require('mongodb');
 
-const url = "mongodb://localhost:27017";
+const url = "mongodb://localhost:27017";// this is a default mongodb URL 
 const dbName = "proj2024MongoDB";
 
 let db;
+//connect to mongodb
 MongoClient.connect(url, { useUnifiedTopology: true })
     .then((client) => {
         db = client.db(dbName);
     })
     .catch((err) => console.log("MongoDB connection error:", err));
 
+    //func to get all lecturers
 const getLecturers = () => {
     return db.collection('lecturers').find().sort({_id: 1 }).toArray();
 };
