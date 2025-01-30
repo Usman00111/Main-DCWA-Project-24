239eaa58d22782be0da1156e8ad3c19b1cf1d113
Usman00111 <g00400065@atu.ie>
2025-01-30 04:06:30 +0000

    Addded  routes to fetch and display lecturers data

---
+++ routes/lecturerRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models/mongoDao');
+
+router.get('/lecturers', (req, res) => {
+    db.getLecturers()
+    .then((lecturers) => res.render('lecturers', {lecturers}))
+    .catch((err) => res.send(err));
+});
+
+module.exports = router;
